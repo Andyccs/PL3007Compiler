35ffbf042b0de00f7c801fcb6f4f5767db6e0bd8
andy ccs <andychong2@gmail.com>
2014-10-01 07:17:24 +0800

    test Arithmetic Expression and RHS Assignmet

--- lab2/src/test/ParserTests.java
+++ lab2/src/test/ParserTests.java
@@ -111,4 +111,27 @@ public class ParserTests {
 		runtest("module Test{ public int function_name() { -abc(a=1)*-3; } }");
 		runtest("module Test{ public int function_name() { -abc(a=1)*def(); } }");
 	}
+	
+	@Test
+	public void testArithmeticExpression(){
+		//test precondition
+		runtest("module Test{ public int function_name() {  } }");
+		
+		runtest("module Test{ public int function_name() { -123*-\"abc\"--true*-a[1]; } }");
+		runtest("module Test{ public int function_name() { -abc(a=1)*-3+-abc(a=1)*def(); } }");
+	}
+	
+	@Test
+	public void testRHSAssignment(){
+		//test precondition
+		runtest("module Test{ public int function_name() {  } }");
+		
+		runtest("module Test{ public int function_name() { a()==true; } }");
+		runtest("module Test{ public int function_name() { a()!=1; } }");
+		runtest("module Test{ public int function_name() { a()<\"abc\"; } }");
+		runtest("module Test{ public int function_name() { a()>a(); } }");
+		runtest("module Test{ public int function_name() { a()<=a(); } }");
+		runtest("module Test{ public int function_name() { a()>=true; } }");
+		runtest("module Test{ public int function_name() { -123*-\"abc\"--true*-a[1]==-abc(a=1)*-3+-abc(a=1)*def(); } }");
+	}
 }
