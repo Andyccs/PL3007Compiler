b9027c2ebb2364a60824b01f3e9752b1de565e09
andy ccs <andychong2@gmail.com>
2014-10-01 06:58:06 +0800

    add test for parenthesis expression

--- lab2/src/test/ParserTests.java
+++ lab2/src/test/ParserTests.java
@@ -80,5 +80,13 @@ public class ParserTests {
 	public void testParenthesisExpression(){
 		//test precondition
 		runtest("module Test{ public int function_name() {  } }");
+		
+		//start test
+		runtest("module Test{ public int function_name() { (); } }",false);
+		runtest("module Test{ public int function_name() { (1+1); } }");
+		runtest("module Test{ public int function_name() { (true+true); } }");
+		runtest("module Test{ public int function_name() { (a[1]+b[c=2]); } }");
 	}
+	
+	
 }
