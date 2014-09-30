0faf71574e5b10a2509de41b41f0f127d72ecc9c
andy ccs <andychong2@gmail.com>
2014-10-01 07:21:58 +0800

    test assignment

--- lab2/src/test/ParserTests.java
+++ lab2/src/test/ParserTests.java
@@ -134,4 +134,14 @@ public class ParserTests {
 		runtest("module Test{ public int function_name() { a()>=true; } }");
 		runtest("module Test{ public int function_name() { -123*-\"abc\"--true*-a[1]==-abc(a=1)*-3+-abc(a=1)*def(); } }");
 	}
+	
+	@Test
+	public void testAssignment(){
+		//test precondition
+		runtest("module Test{ public int function_name() {  } }");
+		
+		runtest("module Test{ public int function_name() { a=1; } }");
+		runtest("module Test{ public int function_name() { a=b=c=3; } }");
+		runtest("module Test{ public int function_name() { a=a()==true==false==123==-3; } }",false);
+	}
 }
