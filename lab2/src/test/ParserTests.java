6b1dcd1246e62fd8c792965c5be7be6db7ad2ca7
andy ccs <andychong2@gmail.com>
2014-10-01 07:07:48 +0800

    test term

--- lab2/src/test/ParserTests.java
+++ lab2/src/test/ParserTests.java
@@ -98,7 +98,17 @@ public class ParserTests {
 		runtest("module Test{ public int function_name() { -true; } }");
 		runtest("module Test{ public int function_name() { -a[1]; } }");
 		runtest("module Test{ public int function_name() { -abc(a=1); } }");
-		runtest("module Test{ public int function_name() { a=-3; } }");
+		runtest("module Test{ public int function_name() { a=-3; } }");	
+	}
+	
+	@Test
+	public void testTerm(){
+		//test precondition
+		runtest("module Test{ public int function_name() {  } }");
 		
+		runtest("module Test{ public int function_name() { -123*-\"abc\"; } }");
+		runtest("module Test{ public int function_name() { -true*-a[1]; } }");
+		runtest("module Test{ public int function_name() { -abc(a=1)*-3; } }");
+		runtest("module Test{ public int function_name() { -abc(a=1)*def(); } }");
 	}
 }
