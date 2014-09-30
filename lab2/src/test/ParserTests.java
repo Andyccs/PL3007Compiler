a7b5c4dc049e211cc905972b62c83561913402ea
andy ccs <andychong2@gmail.com>
2014-10-01 07:04:34 +0800

    test factor

--- lab2/src/test/ParserTests.java
+++ lab2/src/test/ParserTests.java
@@ -88,5 +88,17 @@ public class ParserTests {
 		runtest("module Test{ public int function_name() { (a[1]+b[c=2]); } }");
 	}
 	
-	
+	@Test
+	public void testFactor(){
+		//test precondition
+		runtest("module Test{ public int function_name() {  } }");
+		
+		runtest("module Test{ public int function_name() { -123; } }");
+		runtest("module Test{ public int function_name() { -\"abc\"; } }");
+		runtest("module Test{ public int function_name() { -true; } }");
+		runtest("module Test{ public int function_name() { -a[1]; } }");
+		runtest("module Test{ public int function_name() { -abc(a=1); } }");
+		runtest("module Test{ public int function_name() { a=-3; } }");
+		
+	}
 }
